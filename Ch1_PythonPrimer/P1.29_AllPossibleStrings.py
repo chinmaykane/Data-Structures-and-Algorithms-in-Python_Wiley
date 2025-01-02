5a9ad991344fda8feef00e9fa49f375cc08f5524
Chinmay Kane <145538188+chinmaykane@users.noreply.github.com>
2025-01-02 10:37:40 +0530

    Updates 2Jan25

--- Ch1_PythonPrimer/P1.29_AllPossibleStrings.py
+++ Ch1_PythonPrimer/P1.29_AllPossibleStrings.py
@@ -1,6 +1,12 @@
 """Write a Python program that outputs all possible strings formed by using
 the characters c , a , t , d , o , and g exactly once."""
+from itertools import permutations
 
+def all_possible_strings(*args):
+    perms = []
+    for group in range(len(args)+1):
+        thisperm = list(permutations(args, group))
+        perms.append(thisperm)
+    return perms
 
-def all_possible_strings(liist):
-    
+print(all_possible_strings('a','b','c','d'))
